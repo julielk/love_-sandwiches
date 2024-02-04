152e7f567a3cc669b6c786be69a29ef385a13d6b
juliet lokwiya <j_lokwiya@yahoo.com>
2024-02-04 15:09:38 +0000

    create get_sales_data function

---
+++ run.py
@@ -0,0 +1,38 @@
+
+# python code goes here
+import gspread
+from google.oauth2.service_account import credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+# check if API id working
+#sales = SHEET.worksheet('sales')
+#data = sales.get_all_values()
+
+#print(data)
+
+def get_sales_data():
+  """
+    Get  sales figures to input from the user
+  """
+print("please enter sales data from the last market")
+
+print("Data should be six numbers separated by a comma")
+
+print("Example: 10,20,30,40,50,60\n")
+
+data_str = input("Enter your data here")
+
+print(f"The data provided is {data_str}")
+
+get_sales_data()
