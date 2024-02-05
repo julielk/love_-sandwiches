2de967285857ecd4b01223b9b6fec882466cad15
juliet lokwiya <j_lokwiya@yahoo.com>
2024-02-05 11:04:36 +0000

    Add user request loop repeated request for data until data provided is valid

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
-
-# python code goes here
 import gspread
-from google.oauth2.service_account import credentials
+from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -9,57 +7,47 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-# check if API id working
-#sales = SHEET.worksheet('sales')
-#data = sales.get_all_values()
-
-#print(data)
 
 def get_sales_data():
-  """
-    Get  sales figures to input from the user
-  """
-print("please enter sales data from the last market")
+    """
+    Get sales figures input from the user.
+    """
+    while True:
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
 
-print("Data should be six numbers separated by a comma")
+    data_str = input("Enter your data here: ")
+    sales_data = data_str.split(",")
 
-print("Example: 10,20,30,40,50,60\n")
+    if validate_data(sales_data):
+        print("Data is valid")
+        break
 
-data_str = input("Enter your data here")
-
-print(f"The data provided is {data_str}")
+        return sales_data
 
 
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
-sales_data_str.split(",")
-validate_data(sales)
+        return False
+    return True
 
-#print(sale_data)
 
-def validate_data():
-  """
-  Inside the try ,convert all strings values
-  into integer raise valueError if the string
-  cannot be converted  into int or if there aren't exactly 6 values
-  """
-   # print(values)
-
-   try:
-      [ int(values) for value in values]
-      if len(values) != 6:
-        raise ValueError(
-         f'Exactly 6 values required you provided (len(values))'
-
-        )
-  
-except ValueError as e:
-
-       print(f'Invalid data:(e) please try again')
-
-get_sales_data()
+data = get_sales_data()
